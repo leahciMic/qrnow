9532cbd3862f012fc33d40051893dce733c2b139
Michael Leaney <mleaney@tokbox.com>
2015-05-12 14:56:44 +1000

    Minify js

--- build.js
+++ build.js
@@ -25,8 +25,7 @@ var html = fs.readFileSync('src/html/index.html', 'utf8');
 var css = new CleanCSS().minify(fs.readFileSync('src/css/index.css', 'utf8')).styles;
 
 var js = browserify(['src/js/index.js']).bundle(function(error, data) {
-  // var js = UglifyJS.minify(data.toString('utf8'), {fromString: true});
-  var js = {code: data};
+  var js = UglifyJS.minify(data.toString('utf8'), {fromString: true});
   html = html.replace(':STYLES:', '<style type="text/css">' + css + '</style>');
   html = html.replace(':SCRIPTS:', '<script type="text/javascript">' + js.code + '</script>');
   minimize.parse(html, function(error, data) {
